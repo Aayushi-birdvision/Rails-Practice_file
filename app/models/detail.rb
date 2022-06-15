0dad8fe78975d04658dbb005cf8c8c318dfddb97
Aayushi sharma <aayushisharma06jan@gmail.com>
2022-06-15 18:14:58 +0530

    edit delete and msg added

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,2 +1,4 @@
 class Detail < ApplicationRecord
+  validates :Firstname, presence: true, length: {minimum:6, maximum: 100}
+  validates :Lastname, presence: true, length: {minimum:6, maximum: 100}
 end
